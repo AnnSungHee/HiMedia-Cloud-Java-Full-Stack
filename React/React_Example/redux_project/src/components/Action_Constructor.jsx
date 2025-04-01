1ce2a7f9348e833fa50dcd62460c8f8928863b70
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2025-04-01 14:37:35 +0900

    add: [안성희] "redux_project 새로 생성"

---
+++ React/React_Example/redux_project/src/components/Action_Constructor.jsx
@@ -0,0 +1,10 @@
+import { useDispatch } from "react-redux";
+import { increment, decrement, incrementByAmount } from "./components/counterSlice";
+
+const dispatch = useDispatch();
+// { type: 'counter/increment' } 액션 객체 생성 후 디스패치
+dispatch(increment());
+// { type: 'counter/decrement' } 액션 객체 생성 후 디스패치
+dispatch(decrement());
+// { type: 'counter/incrementByAmount' payload: 5 }
+dispatch(incrementByAmount(5));
